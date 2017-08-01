33cb4f0363e89947944d236fbe53f4da36afddb6
Jessica <jdhpabst@gmail.com>
2017-08-01 14:36:14 -0600

    finished setting up first child for six

--- src/components/Six/ChildOne/ChildOne.js
+++ src/components/Six/ChildOne/ChildOne.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 /* Make a parent component with two children components. This time, do a different component for each of the children. Make it so that clicking a button in one of the children causes something else to happen in the other child. Maybe you could have a variable on state that is a ‘counter’ and clicking the button in one component, and the other component displays the counter, so clicking the button causes the other component’s number to increase  */
 
-import './Six.css';
+import './ChildOne.css';
 
 
-class Six extends Component {
+class ChildOne extends Component {
 
   render() {
     return (
-      <div className="six">
+      <div className="child_one">
 
       </div>
     );
@@ -17,4 +17,4 @@ class Six extends Component {
 }
 
 
-export default Six;
+export default ChildOne;
