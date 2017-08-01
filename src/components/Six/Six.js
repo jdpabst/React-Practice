53976d85e059e28bdaec817263bbf0210607267c
Jessica <jdhpabst@gmail.com>
2017-08-01 14:33:16 -0600

    finished setting  up JS for six

---
+++ src/components/Six/Six.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './HomeSix.css';
+
+
+class Six extends Component {
+
+  render() {
+    return (
+      <div className="six">
+
+      </div>
+    );
+  }
+}
+
+
+export default Six;
