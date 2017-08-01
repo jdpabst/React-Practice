16ad16b9a9ecd0e3660bf96e8bddbb88e5eb7292
Jessica <jdhpabst@gmail.com>
2017-08-01 12:42:13 -0600

    finished with question 3

--- src/components/Three/Three.js
+++ src/components/Three/Three.js
@@ -7,22 +7,53 @@ class Three extends Component {
     constructor(props){
         super(props);
         this.state = {
-            poke: [{}]
+            poke: [{}],
+            userInput: '',
+            filterPoke: [{}]
         }
+        this.filter = this.filter.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     }
 
     componentDidMount() {
         axios.get(`http://pokeapi.co/api/v2/pokemon/`).then(response => {
             this.setState({ 
                 poke: response.data.results,
+                filterPoke: response.data.results
             })
-            console.log(response.data.results)
+            // console.log(response.data.results)
         })
     }
+
+    filter(){
+        let arr = this.state.poke;
+        let val = this.state.userInput;
+        let filterArr = [];
+        val = val.toLowerCase();
+        for(var i = 0; i < arr.length; i++){
+            console.log(arr[i].name)
+        if(arr[i].name.startsWith(val)){
+            filterArr.push(arr[i]);
+            console.log(filterArr)
+        }
+        }
+            this.setState({
+                filterPoke: filterArr,
+            })
+    }
+
+  handleInput(str){
+     this.setState({
+         userInput: str,
+        })   
+    }
+
   render() {
-    let pokemon = this.state.poke;
+    let pokemon = this.state.filterPoke;
     return (
       <div className="three">
+          <input onChange={ (e) => this.handleInput(e.target.value) } placeholder="Search Pokemon"/>
+          <button onClick={ this.filter } type="button">Search</button>
          { pokemon.map( (poke, id) => {
             return <div key={ id } id="poke_info">
               <p>{ poke.name }</p>
