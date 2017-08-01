51bede7244e9eec91bc19907f0da8b4be36e0575
Jessica <jdhpabst@gmail.com>
2017-08-01 14:34:09 -0600

    setting up css for six

--- src/components/Six/Six.js
+++ src/components/Six/Six.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+/* Make a parent component with two children components. This time, do a different component for each of the children. Make it so that clicking a button in one of the children causes something else to happen in the other child. Maybe you could have a variable on state that is a ‘counter’ and clicking the button in one component, and the other component displays the counter, so clicking the button causes the other component’s number to increase  */
 import './HomeSix.css';
 
 
